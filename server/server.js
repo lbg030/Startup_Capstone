6b2033e2a0aa9de15ac6d708b29488368c8fd017
tutu772 <bichoi0715@gmail.com>
2022-03-26 03:07:11 +0900

    Database Connect

--- server/server.js
+++ server/server.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 const test = require("./Router/test");
+const config = require("./config/config.json")['development'];
+const {Pool} = require('pg');
+
+const pg = new Pool(config);
+
+pg.connect(err => {
+  if(err) console.log(err);
+  else {
+    console.log("데이터베이스 연결 성공");
+  }
+})
 
 app.use("/api", test);
 
