9ab6ad9f625ef616632cd3ba2daa91fa42ebd360
heon su <ekdepd2@gmail.com>
2022-04-03 02:09:42 +0900

    first commit

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-function check(age){
-    if(age>=20)
-    {return true;}
-    else
-    return false;
-}
+const title = document.getElementById("title");
 
-const age = prompt("how old are you?");
+title.innerText="Got you!";
 
-
-if (check(age) == true)
-{console.log("he can drink achol")}
-else
-console.log("he can't drink achol");
+console.dir(title);
