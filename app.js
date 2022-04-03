a5710fbf2543ac97f23c23e36f9405503eb6de04
heon su <ekdepd2@gmail.com>
2022-04-03 19:58:43 +0900

    third commit

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const h1 = document.querySelector(".hello h1");
-
-console.log(h1);
+const h2 = document.querySelector(".hello h2");
 
 
 function HandleTitleClick(){
     h1.innerText = "Well Clicked!";
+}
+
+function HandleTitleClick2(){
+    const currentColor = h2.style.color;
+    let newColor;
+
+    if (h2.style.color === "blue"){
+        newColor = "tomato"
+    }
+    else{
+        newColor = "blue"
+    }
+    h2.style.color = newColor;
+    ;
 }
 
 function HandleTitleMouseEnter(){
@@ -24,10 +37,22 @@ function HandleWindowCopy(){
    alert("You copied this it's illigal");
 }
 
+function HandleWindowOnline(){
+    alert("All good");
+ }
+
+ function HandleWindowOffline(){
+    alert("SOS there is no connection");
+ }
 
 h1.addEventListener("click",HandleTitleClick);
 h1.addEventListener("mouseenter",HandleTitleMouseEnter);
 h1.addEventListener("mouseleave",HandleTitleMouseLeave);
 
+h2.addEventListener("click",HandleTitleClick2);
+
+
 window.addEventListener("resize",HandleWindowResize);
 window.addEventListener("copy",HandleWindowCopy);
+window.addEventListener("online",HandleWindowOnline);
+window.addEventListener("offline",HandleWindowOffline);
