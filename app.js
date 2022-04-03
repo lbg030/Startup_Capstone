23be3cb320fd32ea68947e24c6d3de16fd0fa9f2
heon su <ekdepd2@gmail.com>
2022-04-03 19:36:41 +0900

    second commit

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
-const title = document.getElementById("title");
+const h1 = document.querySelector(".hello h1");
 
-title.innerText="Got you!";
+console.log(h1);
 
-console.dir(title);
+
+function HandleTitleClick(){
+    h1.innerText = "Well Clicked!";
+}
+
+function HandleTitleMouseEnter(){
+    h1.innerText = "Mouse Is here!";
+}
+
+function HandleTitleMouseLeave(){
+    h1.innerText = "Mouse Is gone!";
+}
+
+function HandleWindowResize(){
+    const randomColor = Math.floor(Math.random()*0xffffff).toString(16);
+   document.body.style.backgroundColor = "#"+randomColor;;
+}
+
+function HandleWindowCopy(){
+   alert("You copied this it's illigal");
+}
+
+
+h1.addEventListener("click",HandleTitleClick);
+h1.addEventListener("mouseenter",HandleTitleMouseEnter);
+h1.addEventListener("mouseleave",HandleTitleMouseLeave);
+
+window.addEventListener("resize",HandleWindowResize);
+window.addEventListener("copy",HandleWindowCopy);
