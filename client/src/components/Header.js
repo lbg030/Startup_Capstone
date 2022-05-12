32db4815b341f2b6a474932bc73520635cebbdfb
ByeongGwon <lee971009@naver.com>
2022-05-12 19:54:54 +0900

    할인 정보 수정

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,9 +9,11 @@ import {
 const imagestyle = {
   height: "10vh",
   width: "10vh",
+  margin: "10 10 10 10",
   // float: "left",
   // border: "1px solid white",
 };
+
 const Header = () => (
     <section id="header" className="wrapper">
       <div id="logo">
@@ -27,6 +29,16 @@ const Header = () => (
         <p>Steam Community Site</p>
       </div>
       <nav id="nav">
+      
+          {/* <img
+            src="images/로고.png"
+            style={imagestyle}
+            align = 'left'
+            display = 'flex'
+            href = "index.html"
+            
+          />
+          <p align='left'><a href="index.html">스팀팩 </a></p> */}
         <ul>
           <li className="current">
             <a href="index.html">할인 정보</a>
@@ -35,10 +47,10 @@ const Header = () => (
             <Link to="/boards/2018045141">게시판</Link>
           </li>
           <li>
-            <a href="left-sidebar.html">MBTI별 게임 추천</a>
+            <a href="recommendMBTI.html">MBTI별 게임 추천</a>
           </li>
           <li>
-            <a href="right-sidebar.html">
+            <a href="ranking.html">
               스팀 트렌드 [## 게임 랭킹 ]
             </a>
           </li>
--- client/src/components/Highlights.js
+++ client/src/components/Highlights.js
@@ -2,16 +2,16 @@ import React from "react";
 
 const Highlights = () => (
   <section id="highlights" className="wrapper style3">
-    <div className="title">The Endorsements</div>
+    <div className="title">할인 정보</div>
     <div className="container">
       <div className="row aln-center">
         <div className="col-4 col-12-medium">
           <section className="highlight">
             <a href="#" className="image featured">
-              <img src="images/pic02.jpg" alt="" />
+              <img src="images/배그.png" alt="" />
             </a>
             <h3>
-              <a href="#">Aliquam diam consequat</a>
+              <a href="#">배틀 그라운드</a>
             </h3>
             <p>
               Eget mattis at, laoreet vel amet sed velit aliquam
@@ -20,20 +20,21 @@ const Highlights = () => (
             </p>
             <ul className="actions">
               <li>
-                <a href="#" className="button style1">
-                  Learn More
+                <a href="https://store.steampowered.com/app/578080/PUBG_BATTLEGROUNDS/" className="button style1">
+                  구매하러 가기
                 </a>
               </li>
             </ul>
           </section>
         </div>
+        
         <div className="col-4 col-12-medium">
           <section className="highlight">
             <a href="#" className="image featured">
-              <img src="images/pic03.jpg" alt="" />
+              <img src="images/에펙.png" alt="" />
             </a>
             <h3>
-              <a href="#">Nisl adipiscing sed lorem</a>
+              <a href="#">에이펙스레전드</a>
             </h3>
             <p>
               Eget mattis at, laoreet vel amet sed velit aliquam
@@ -42,30 +43,92 @@ const Highlights = () => (
             </p>
             <ul className="actions">
               <li>
-                <a href="#" className="button style1">
-                  Learn More
+                <a href="https://store.steampowered.com/app/1172470/Apex_Legends/" className="button style1">
+                  구매하러 가기
                 </a>
               </li>
             </ul>
           </section>
         </div>
+
         <div className="col-4 col-12-medium">
           <section className="highlight">
             <a href="#" className="image featured">
-              <img src="images/pic04.jpg" alt="" />
+              <img src="images/엘든링.png" alt="" />
             </a>
             <h3>
-              <a href="#">Mattis tempus lorem</a>
+              <a href="#">엘든링</a>
             </h3>
             <p>
-              Eget mattis at, laoreet vel amet sed velit aliquam
-              diam ante, dolor aliquet sit amet vulputate mattis
-              amet laoreet lorem.
+            ₩70,000 -{'>'}  ₩64,800
             </p>
             <ul className="actions">
               <li>
-                <a href="#" className="button style1">
-                  Learn More
+                <a href="https://store.steampowered.com/app/1245620/ELDEN_RING/" className="button style1">
+                  구매하러 가기
+                </a>
+              </li>
+            </ul>
+          </section>
+        </div>
+
+        <div className="col-4 col-12-medium">
+          <section className="highlight">
+            <a href="#" className="image featured">
+              <img src="images/gta5.png" alt="" />
+            </a>
+            <h3>
+              <a href="#">GTA5</a>
+            </h3>
+            <p>
+            ₩60,000 -{'>'}  ₩49,200
+            </p>
+            <ul className="actions">
+              <li>
+                <a href="https://store.steampowered.com/app/271590/Grand_Theft_Auto_V/" className="button style1">
+                  구매하러 가기
+                </a>
+              </li>
+            </ul>
+          </section>
+        </div>
+
+        <div className="col-4 col-12-medium">
+          <section className="highlight">
+            <a href="#" className="image featured">
+              <img src="images/뱀파이어생존자.png" alt="" />
+            </a>
+            <h3>
+              <a href="#">뱀파이어 생존자</a>
+            </h3>
+            <p>
+            ₩5,000 -{'>'}  ₩3,300
+            </p>
+            <ul className="actions">
+              <li>
+                <a href="https://store.steampowered.com/app/1794680/Vampire_Survivors/" className="button style1">
+                  구매하러 가기
+                </a>
+              </li>
+            </ul>
+          </section>
+        </div>
+
+        <div className="col-4 col-12-medium">
+          <section className="highlight">
+            <a href="#" className="image featured">
+              <img src="images/카스.png" alt="" />
+            </a>
+            <h3>
+              <a href="#">카운터 스트라이크</a>
+            </h3>
+            <p>
+              ₩20,000 -{'>'}  ₩17,200
+            </p>
+            <ul className="actions">
+              <li>
+                <a href="https://store.steampowered.com/app/730/CounterStrike_Global_Offensive/" className="button style1">
+                  구매하러 가기
                 </a>
               </li>
             </ul>
